8c41d6d755a5684cb27ee04237a06b3933d3c42d
Mahfuzur Rahman <mahfuz468865@gmail.com>
2021-08-08 04:10:35 +0600

    Now books can be added

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-let myLibrary = [];
-
-class Book {
-  constructor(name, author, pages, read) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-      return (name + " " + "by" + " " + author + ", " + pages + " " + "pages" + ", " + read);
-    };
-  }
-}
-
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, "not read yet");
-
-console.log(theHobbit.info());
-
-function addBookToLibrary() {
-
-}
-
-function displayEachBook() {
-
-}
-
 /* Display Add Book Popup */
 function displayPopUp() {
   document.getElementById('popup').style.display = "block";
@@ -32,4 +6,59 @@ function displayPopUp() {
 /* Close Add Book Popup */
 function closePopUp() {
   document.getElementById('popup').style.display = "none";
+}
+
+let myLibrary = [];
+
+/* Book constructor */
+class Book {
+  constructor(name, author, pages) {
+    this.title = name;
+    this.author = author;
+    this.pages = pages;
+    this.info = function () {
+      return (name + " " + "by" + " " + author + ", " + pages + " " + "pages");
+    };
+  }
+}
+
+
+/* Take Book Info and Initiate displayEachBook() */
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+  //Prevent default submit action
+  e.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  //const read = document.querySelector('#yeas').value;
+  //const notRead = document.querySelector('#notyet').value;
+
+  const book = new Book(title, author, pages);
+
+  displayEachBook(book);
+
+  /* Display Book in the Table */
+  function displayEachBook() {
+    const list = document.querySelector('#body');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.pages}</td>
+  <td>demo</td>
+  <td>demo</td>
+  <td>demo</td>
+
+`
+    list.appendChild(row);
+
+  }
+
+})
+
+function addBookToLibrary() {
+
 }
