59f9be5689790cb11a9dd159dd726c2b42ad86c8
Mahfuzur Rahman <mahfuz468865@gmail.com>
2021-08-08 02:10:09 +0600

    Basic UI is done

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 let myLibrary = [];
 
-function Book(name, author, pages, read ){
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function(){
-    return (name + " " + "by" + " " + author + ", " + pages + " " + "pages" + ", " + read); 
+class Book {
+  constructor(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+      return (name + " " + "by" + " " + author + ", " + pages + " " + "pages" + ", " + read);
+    };
   }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, "not read yet" );
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, "not read yet");
 
 console.log(theHobbit.info());
 
@@ -22,3 +24,12 @@ function displayEachBook() {
 
 }
 
+/* Display Add Book Popup */
+function displayPopUp() {
+  document.getElementById('popup').style.display = "block";
+}
+
+/* Close Add Book Popup */
+function closePopUp() {
+  document.getElementById('popup').style.display = "none";
+}
