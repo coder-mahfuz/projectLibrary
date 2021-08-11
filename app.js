194f30419f29c883a4f63a35142492dffae7b9f1
Mahfuzur Rahman <mahfuz468865@gmail.com>
2021-08-11 22:27:51 +0600

    Read/Not field is working now

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ let myLibrary = [];
 
 /* Book constructor */
 class Book {
-  constructor(name, author, pages) {
+  constructor(name, author, pages, status) {
     this.title = name;
     this.author = author;
     this.pages = pages;
-    this.info = function () {
+    this.status = status;
+    /*this.info = function () {
       return (name + " " + "by" + " " + author + ", " + pages + " " + "pages");
-    };
+    };*/
   }
 }
 
@@ -32,10 +33,11 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const pages = document.querySelector('#pages').value;
-  //const read = document.querySelector('#yeas').value;
-  //const notRead = document.querySelector('#notyet').value;
+  const checked = document.querySelector('input[type="radio"]:checked');
+  const status = checked.value;
 
-  const book = new Book(title, author, pages);
+
+  const book = new Book(title, author, pages, status);
 
   displayEachBook(book);
 
@@ -48,17 +50,51 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   <td>${book.title}</td>
   <td>${book.author}</td>
   <td>${book.pages}</td>
-  <td>demo</td>
-  <td>demo</td>
-  <td>demo</td>
+  <td>${book.status}</td>
+  <td>Update</td>
+  <td class="remove">Remove</td>
 
 `
     list.appendChild(row);
 
+    clearFields()
   }
 
 })
 
+
+
+/**
+ * Add Books to Library
+ */
 function addBookToLibrary() {
 
 }
+
+
+/**
+ * Clear Fields on Submition
+ */
+function clearFields() {
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+  document.querySelector('#pages').value = '';
+}
+
+/**
+ * Remove a Book function
+ */
+
+function removeBook(remove) {
+  if (remove.classList.contains('remove')) {
+    remove.parentElement.remove();
+  }
+}
+
+/**
+ * Get Click Target and Run removeBook Functiom
+ */
+
+document.querySelector('#body').addEventListener('click', (e) => {
+  removeBook(e.target);
+})
